0d108f8acdc59a95e1b20b13896c02599c39db01
pendimadyana <pendimadyana@gmail.com>
2020-12-26 23:22:28 +0700

    move ItemCard to separated file

--- lib/screens/home/components/body.dart
+++ lib/screens/home/components/body.dart
@@ -3,6 +3,7 @@ import 'package:olshop_ui/constants.dart';
 import 'package:olshop_ui/models/Product.dart';
 
 import 'categories.dart';
+import 'item_card.dart';
 
 class Body extends StatelessWidget {
   @override
@@ -21,47 +22,22 @@ class Body extends StatelessWidget {
           ),
         ),
         Categories(),
-        ItemCard(),
-        Padding(
-          padding: const EdgeInsets.symmetric(vertical: kDefaultPaddin / 4),
-          child: Text(
-            products[0].title,
-            style: TextStyle(color: kTextLightColor),
+        Expanded(
+            child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: kDefaultPaddin),
+          child: GridView.builder(
+            itemCount: products.length,
+            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 2,
+              childAspectRatio: 0.75,
+              mainAxisSpacing: kDefaultPaddin,
+              crossAxisSpacing: kDefaultPaddin,
+            ),
+            itemBuilder: (context, index) => ItemCard(
+              product: products[index],
+            ),
           ),
-        ),
-        Text(
-          '\$250',
-          style: TextStyle(fontWeight: FontWeight.bold),
-        )
-      ],
-    );
-  }
-}
-
-class ItemCard extends StatelessWidget {
-  final Product product;
-  final Function press;
-
-  const ItemCard({
-    Key key,
-    this.product,
-    this.press,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        Container(
-          padding: EdgeInsets.all(kDefaultPaddin),
-          height: 180,
-          width: 160,
-          decoration: BoxDecoration(
-              color: products[0].color,
-              borderRadius: BorderRadius.circular(16)),
-          child: Image.asset(products[0].image),
-        ),
+        )),
       ],
     );
   }
