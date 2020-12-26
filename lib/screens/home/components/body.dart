d2ba74fe7dbb490844698ae4012380e332f746fd
pendimadyana <pendimadyana@gmail.com>
2020-12-26 23:05:18 +0700

    extract ItemCard to separated widget

--- lib/screens/home/components/body.dart
+++ lib/screens/home/components/body.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:olshop_ui/constants.dart';
+import 'package:olshop_ui/models/Product.dart';
 
 import 'categories.dart';
 
@@ -7,6 +8,7 @@ class Body extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
       children: [
         Padding(
           padding: const EdgeInsets.symmetric(horizontal: kDefaultPaddin),
@@ -19,6 +21,47 @@ class Body extends StatelessWidget {
           ),
         ),
         Categories(),
+        ItemCard(),
+        Padding(
+          padding: const EdgeInsets.symmetric(vertical: kDefaultPaddin / 4),
+          child: Text(
+            products[0].title,
+            style: TextStyle(color: kTextLightColor),
+          ),
+        ),
+        Text(
+          '\$250',
+          style: TextStyle(fontWeight: FontWeight.bold),
+        )
+      ],
+    );
+  }
+}
+
+class ItemCard extends StatelessWidget {
+  final Product product;
+  final Function press;
+
+  const ItemCard({
+    Key key,
+    this.product,
+    this.press,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Container(
+          padding: EdgeInsets.all(kDefaultPaddin),
+          height: 180,
+          width: 160,
+          decoration: BoxDecoration(
+              color: products[0].color,
+              borderRadius: BorderRadius.circular(16)),
+          child: Image.asset(products[0].image),
+        ),
       ],
     );
   }
