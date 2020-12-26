353ad9615e0f3d67e352a598c4c4f809ff184b30
pendimadyana <pendimadyana@gmail.com>
2020-12-26 22:36:16 +0700

    add categories-text

--- lib/screens/home/components/body.dart
+++ lib/screens/home/components/body.dart
@@ -15,8 +15,32 @@ class Body extends StatelessWidget {
                 .headline5
                 .copyWith(fontWeight: FontWeight.bold),
           ),
-        )
+        ),
+        Categories(),
       ],
+    );
+  }
+}
+
+class Categories extends StatefulWidget {
+  @override
+  _CategoriesState createState() => _CategoriesState();
+}
+
+class _CategoriesState extends State<Categories> {
+  List<String> categories = ['Hand bag', 'Jewellery', 'Footwear', 'Dresses'];
+
+  int selectedIndex = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 25,
+      child: ListView.builder(
+        scrollDirection: Axis.horizontal,
+        itemCount: categories.length,
+        itemBuilder: (context, index) => Text(categories[index]),
+      ),
     );
   }
 }
